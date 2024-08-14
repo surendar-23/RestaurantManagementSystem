cd72a828d76c5130cf3f0d0e818a58d99687cc44
Surendar S <surendar.senthilvelan@gmail.com>
2024-08-14 13:14:01 +0530

    Removed old packages

--- src/main/java/com/restaurantmanagementsystem/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restaurantmanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restaurantmanagementsystem.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
--- src/main/java/com/restaurantmanagementsystem/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restaurantmanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.restaurantmanagementsystem.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
