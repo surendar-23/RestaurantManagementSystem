43db1f155251b81ddbdecf6a66dfb0d0ca9d58f3
Surendar S <surendar.senthilvelan@gmail.com>
2024-09-01 12:56:10 +0530

    Final Commit

--- src/main/java/com/example/restaurantmanagementsystem/controller/CategoryController.java
+++ src/main/java/com/example/restaurantmanagementsystem/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.restaurantmanagementsystem.entity.Category;
 import com.example.restaurantmanagementsystem.service.CategoryService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
