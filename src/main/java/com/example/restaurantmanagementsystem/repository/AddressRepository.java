b40b24820d98493cf3617aec1852cb7a65fe97c2
Surendar S <surendar.senthilvelan@gmail.com>
2024-09-15 12:07:33 +0530

    Removed unnecessary components

--- src/main/java/com/example/restaurantmanagementsystem/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restaurantmanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.restaurantmanagementsystem.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
--- src/main/java/com/example/restaurantmanagementsystem/repository/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.restaurantmanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.restaurantmanagementsystem.entity.Restaurant;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
